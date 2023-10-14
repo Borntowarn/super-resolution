f93713d463bedc1611b66b13dbd599ea760a9655
Михаил Козлов <kozlovmihail02@mail.ru>
2023-10-14 11:46:18 +0300

    Add requirements for publish video from console

--- publish_video.py
+++ publish_video.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from rabbit.connector import Connector
-from rabbit.publisher import RabbitPublisher
+from services.rabbit import RabbitConnector
+from services.rabbit import RabbitPublisher
 
 import os
 import argparse
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description='Videos to RabbitMQ')
 parser.add_argument('input', default='videos', type=str, help='Input dir/file relative !!!Storage!!! folder')
 
 if __name__ == '__main__':
-    con = Connector(env_path='configs/rabbit.env')
+    con = RabbitConnector(env_path='configs/rabbit.env')
     connection, channel, input_queue, output_queue = con.connect()
     
     pub = RabbitPublisher(channel, connection, input_queue)
